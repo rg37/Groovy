7172e17976e134dc55efacbb6de397ce1a6d5058
Anonymous <anonymous@noemail.com>
2016-10-17 11:09:43 +0000

    d3 ex 1

---
+++ Groovies/0301_float_equality.groovy
@@ -0,0 +1,16 @@
+double d1 = 1.255
+double d2 = d1 + 7 - 4 - 3
+if (d1 == d2) {
+    println("1.255 is equal to 1.255 plus 7 minus 7");
+  } else {
+    println("1.255 is NOT equal to 1.255 plus 7 minus 7");
+}
+
+println "d1: $d1   d2: $d2"
+println "my revised code: "
+
+if ((d1-d2).abs()<0.0000000001) {
+    println("1.255 is equal to 1.255 plus 7 minus 7");
+  } else {
+    println("1.255 is NOT equal to 1.255 plus 7 minus 7");
+}
