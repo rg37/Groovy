2bdd2ecd19769ca1c17dfe93c9f3544b2e8a75cb
Mr Robert Gates <rgates01@dcs.bbk.ac.uk>
2016-10-17 18:50:15 +0100

    tweak to float inequality

--- Groovies/0301_float_equality.groovy
+++ Groovies/0301_float_equality.groovy
@@ -9,7 +9,7 @@ if (d1 == d2) {
 println "d1: $d1   d2: $d2"
 println "my revised code: "
 
-if ((d1-d2).abs()<0.0000000001) {
+if ((d1-d2).abs()< 10E-6) {
     println("1.255 is equal to 1.255 plus 7 minus 7");
   } else {
     println("1.255 is NOT equal to 1.255 plus 7 minus 7");
