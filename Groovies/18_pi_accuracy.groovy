e4728d8d94f3ff8db2581de571fc59e01209aa6c
Anonymous <anonymous@noemail.com>
2016-10-17 08:04:54 +0000

    tweak to pi accuracy

--- Groovies/18_pi_accuracy.groovy
+++ Groovies/18_pi_accuracy.groovy
@@ -1,4 +1,4 @@
-//calculate how many terms to get pi accurate to n terms
+//calculate how many terms to get pi accurate to n sig figs
 int i=0, terms=1;
 double pi=0, realpi=Math.PI;
 boolean accuracymet = false;
@@ -11,20 +11,22 @@ Type 'n' here >> ''';
 
 int n = Integer.parseInt(System.console().readLine());
 while (!accuracymet && terms<100000000) { //stop after 100,000,000 terms
-  i=terms-1
+  i=terms-1  //first term when i=0
   pi = pi + 4*(((-1)**i)/((2*i)+1));
-  if ((realpi-pi).abs()<(1*(10**(-(n))))) {
-    //println (1*(10**(-n)));   //debug check line
+  if (realpi.trunc(n-1)==pi.trunc(n-1)) {
     accuracymet = true;
+  }
+  if (terms%1000000==0){
+    println "At ${terms/1000000} million cycles calc is $pi"
   }
   terms = terms+1;
 }  
 println "Your pi is $pi"
 println "Real pi is $realpi"
 if (terms == 100000000) {
-  println "Accuracy to $n s.f. could not be achieved with 1,000,000,000 terms"
+  println "Accuracy to $n s.f. could not be achieved after 100,000,000 terms"
 } else {
-  println "It took $terms terms to get $n s.f. accuracy"
+  println "Precisely ${terms-1} terms is the first point of $n s.f. accuracy"
 }
 println '''
 =========== END ==========
